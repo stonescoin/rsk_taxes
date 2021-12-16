d3fe615191197469a64cbb8a54fe3986ec790555
Ruben <ruben.perez@insparx.com>
2021-12-16 15:59:29 +0100

    adding fish pool contract

--- src/main/java/com/ruho/rsk/utils/ContractSpecs.java
+++ src/main/java/com/ruho/rsk/utils/ContractSpecs.java
@@ -7,7 +7,8 @@ public enum ContractSpecs {
     BTC_XUSD_POOL("0x6f96096687952349dd5944e0eb1be327dcdeb705", "BTC", "XUSD"),
     BTC_ETH_POOL("0xf41ed702df2b84ace02772c6a0d8ae46465aa5f4", "BTC", "ETH"),
     BTC_BNB_POOL("0x8f3d24ab3510294f1466aa105f78901b90d79d4d", "BTC", "BNB"),
-    BTC_SOV_POOL("0x09c5faf7723b13434abdf1a65ab1b667bc02a902", "BTC", "SOV");
+    BTC_SOV_POOL("0x09c5faf7723b13434abdf1a65ab1b667bc02a902", "BTC", "SOV"),
+    BTC_FISH_POOL("0x35a74a38fd7728f1c6bc39ae3b18c974b7979ddd", "BTC", "FISH");
 
 
     private final String quoteSymbol;
