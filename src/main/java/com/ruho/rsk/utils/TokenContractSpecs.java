e2860afd02b0e22c1066fdd6bfe6a5b3321e61d3
stonescoin <84503835+stonescoin@users.noreply.github.com>
2022-04-11 12:06:39 +0200

    Update TokenContractSpecs.java

--- src/main/java/com/ruho/rsk/utils/TokenContractSpecs.java
+++ src/main/java/com/ruho/rsk/utils/TokenContractSpecs.java
@@ -13,6 +13,7 @@ public enum TokenContractSpecs {
     FISH("0x055a902303746382fbb7d18f6ae0df56efdc5213", "FISH"),
     BNB("0x6d9659bdf5b1a1da217f7bbaf7dbaf8190e2e71b", "BNB"),
     ETH("0x1d931bf8656d795e50ef6d639562c5bd8ac2b78f", "ETH");
+    rUSDT("0xef213441a85df4d7acbdae0cf78004e1e486bb96", "rUSDT");
 
     private final String address;
     private final String symbol;
